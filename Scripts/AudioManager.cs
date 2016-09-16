8e0281a461c3bb604eec641d8182af46d038e643
César Rodrigues de Oliveira <cesar.rso@hotmail.com>
2016-09-16 19:08:02 -0300

    Add files via upload

---
+++ Scripts/AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+	public Slider VolumeSlider;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ChangeVolumeAll ();
+	}
+
+	public void ChangeVolumeAll(){
+
+		AudioListener.volume = VolumeSlider.value;
+	}
+}
